ceb930ea09935f4b6d6027fc6532507533ec12cc
hi <hi@hisune.ccom>
2023-12-09 16:40:10 +0800

    解决报错问题

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (!tab || !tab.url || tab.url.startsWith("chrome")) return undefined;
+
+    console.log(tab.url);
     if (changeInfo.status === 'complete' && tab.active) {
         chrome.scripting.insertCSS({
             target: {tabId: tabId, allFrames: true},
